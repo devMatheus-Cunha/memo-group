1930132fdde6ecbbc243d1d0fd540897bec8f25f
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-14 12:31:02 -0300

    feat: structure base for Containers and VIews of MeMoControl

---
+++ src/containers/memoLab/products/MeMoControl/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// styles
+// import { Container } from "./styles"
+
+export default function MeMoControlContainer() {
+  return (
+    <div>
+      MeMoControl
+    </div>
+  )
+}
---
+++ src/view/memoLab/products/MeMoControl/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MeMoControl() {
+  return (
+    <div>
+      MeMoControl
+    </div>
+  )
+}
