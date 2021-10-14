52140f4da30fc0a8b53a66c3e005e99af7e4db67
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-14 12:31:13 -0300

    feat: structure base for Containers and VIews of MeMoFinance

---
+++ src/containers/memoLab/products/MeMoFinance/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// styles
+// import { Container } from "./styles"
+
+export default function MeMoFinanceContainer() {
+  return (
+    <div>
+      MeMoFinance
+    </div>
+  )
+}
---
+++ src/view/memoLab/products/MeMoFinance/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MeMoFinance() {
+  return (
+    <div>
+      MeMoFinance
+    </div>
+  )
+}
