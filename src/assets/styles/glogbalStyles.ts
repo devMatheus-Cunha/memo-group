0ae25fd516d6368e5800420c4b75e705cb820594
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-15 18:17:28 -0300

    feat: add and applying new fonts

--- src/assets/styles/glogbalStyles.ts
+++ src/assets/styles/glogbalStyles.ts
@@ -21,6 +21,7 @@ export const GlobalStyle = createGlobalStyle`
     --white: #ffffff;
     --black: #000
   }
+  
   * {
     margin: 0;
     padding: 0;
@@ -37,21 +38,26 @@ export const GlobalStyle = createGlobalStyle`
   body {
     background: var(--background);
     -webkit-font-smoothing: antialiased;
-  }
-  body, input, textarea, button {
     font-family: 'Poppins', sans-serif;
+  }
+  
+  body, input, textarea, button {
     font-weight: 400;
   }
+
   h1, h2, h3, h4, h5, h6, strong {
     font-weight: 600;
   }
+
   button {
     cursor: pointer;
   }
+
   [disabled] {
     opacity: 0.6;
     cursor: not-allowed;
   }
+
   .react-modal-overlay {
     background: rgba(0, 0, 0, 0.5);
     position: fixed;
@@ -63,6 +69,7 @@ export const GlobalStyle = createGlobalStyle`
     align-items: center;
     justify-content: center;
   }
+
   .react-modal-content {
     width: 100%;
     max-width: 576px;
