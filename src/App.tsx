c8d415fda5c97e600e074ae10cd22d559033e937
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-17 12:03:06 -0300

    feat: applying  component form with formik

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,39 @@ import React from "react";
 // components
 import { FormComponent } from "./components/FormComponent";
 
+// formik
+import { Field } from "formik";
+
 function App() {
-  const handleSubmit = (): void => {
-    {
-      console.log("yes subimit");
-    }
+  const handleSubmit = (values: any) => {
+    console.log(values);
   };
   return (
     <>
-      <FormComponent />
+      <FormComponent
+        onSubmit={handleSubmit}
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+        }}
+      >
+        <label htmlFor="firstName">First Name</label>
+        <Field id="firstName" name="firstName" placeholder="John" />
+
+        <label htmlFor="lastName">Last Name</label>
+        <Field id="lastName" name="lastName" placeholder="Doe" />
+
+        <label htmlFor="email">Email</label>
+        <Field
+          id="email"
+          name="email"
+          placeholder="john@acme.com"
+          type="email"
+        />
+
+        <button type="submit">Submit</button>
+      </FormComponent>
     </>
   );
 }
