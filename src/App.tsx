052697d2285160ed1fa464c4f93530153c42ce12
Matheus Gomes <gomesprogramacao@gmail.com>
2021-10-17 21:16:44 -0300

    style: clear code App

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,8 @@
 import React from "react";
 
-// components
-import { FormComponent } from "./components/FormComponent";
-import { InputComponent } from "./components/InputComponent";
-import { ButtonComponent } from "./components/ButtonComponent";
-import { Form, Formik } from "formik";
-
 function App() {
-  const handleSubmit = (values: any) => {
-    console.log(values);
-  };
-  const person = {
-    name: "",
-    age: "",
-    profession: "",
-  };
-
   return (
-    <FormComponent onSubmit={handleSubmit} initialValues={person}>
-        <>
-          <InputComponent
-            id="name"
-            name="name"
-            placeholder="Digite seu nome..."
-            label="Nome"
-            width="100"
-            type="text"
-          />
-          <InputComponent
-            id="age"
-            name="age"
-            placeholder="Digite sua idade..."
-            label="Idade"
-            width="100"
-            type="number"
-          />
-          <InputComponent
-            id="profession"
-            name="profession"
-            placeholder="Digite sua profissão..."
-            label="Profissão"
-            width="100"
-            type="text"
-          />
-          <ButtonComponent
-            text="salvar"
-            type="submit"
-            icon="save"
-          />
-        </>
-    </FormComponent>
+    <h1>Home</h1>
   );
 }
 
