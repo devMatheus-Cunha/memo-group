d70366c8e1e9ba04ea95fa69c2f729ff2d68d0f3
Matheus Gomes <gomesprogramacao@gmail.com>
2021-11-03 19:53:46 -0300

    feat: add base colors

---
+++ src/assets/styles/styles.d.ts
@@ -0,0 +1,27 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    cyan50: string,
+    cyan100: string,
+    cyan200: string,
+    cyan300: string,
+    cyan400: string,
+    cyan500: string,
+    cyan600: string,
+    cyan700: string,
+    cyan800: string,
+    cyan900: string,
+
+    error: string,
+    sucess: string,
+    warn: string,
+
+    background: string,
+    white: string,
+    black: string,
+    gray400: string,
+    gray600: string,
+    grayhover: string,
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
 
 // import App
 import App from "./App";
+
+// theme base colors
+import baseColors from "./assets/styles/baseColors";
 
 // global styles
 import { GlobalStyle } from "./assets/styles/glogbalStyles";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
-		<GlobalStyle />
+		<ThemeProvider theme={baseColors}>
+			<App />
+			<GlobalStyle />
+		</ThemeProvider>
 	</React.StrictMode>,
 	document.getElementById("root"),
 );
