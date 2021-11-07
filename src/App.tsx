9c9ddc768d465f55a6ce6994834531d50d2919fd
Matheus Gomes <gomesprogramacao@gmail.com>
2021-11-07 14:25:36 -0300

    fix: code and color svg

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,15 @@ import CardModel from "./components/CardModel";
 // routes
 import Routes from "./routes/routes";
 
+// images
+import logoImg from "./assets/img/memo-share/img-memoshare-module.svg"
+
 function App() {
 	return (
 		<>
 			<Routes />
 			<CardModel
+				logo={logoImg}
 				title="MeMo Share"
 				subtitle="	Faça sua lista de conteudo e compartilhe com todos seus amigos e
 						leve o maximo de conteudo a todos"
