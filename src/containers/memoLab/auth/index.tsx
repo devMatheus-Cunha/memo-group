2dfb13194e1c3e7dc225eff43e8b09435e9ea6b1
Matheus Gomes <gomesprogramacao@gmail.com>
2021-11-07 21:14:10 -0300

    fix: import authContainer

--- src/containers/memoLab/auth/index.tsx
+++ src/containers/memoLab/auth/index.tsx
@@ -1,12 +1,18 @@
 import React from "react"
 
+// components
+import { HeaderComponent } from "../../../components/HeaderComponent"
+
+// images
+import logoImg from "../../../assets/img/logo/logo-memo-lab.svg"
+
 // styles
-// import { Container } from "./styles"
+import { Container } from "./styles"
 
 export default function AuthContainer() {
 	return (
-		<div>
-			auth
-		</div>
+		<Container>
+			<HeaderComponent logo={logoImg} />
+		</Container>
 	)
 }
